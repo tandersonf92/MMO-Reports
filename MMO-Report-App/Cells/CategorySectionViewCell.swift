9ca7761d64f45814d4abb912cb48f74699be291b
tandersonf <anderson.oliveira@cora.com.br>
2024-02-19 08:46:29 -0300

    wip

--- MMO-Report-App/Cells/CategorySectionViewCell.swift
+++ MMO-Report-App/Cells/CategorySectionViewCell.swift
@@ -5,19 +5,19 @@ protocol CategorySectionViewCellDelegateProtocol: AnyObject {
 }
 
 final class CategorySectionViewCell: UICollectionViewCell {
-    
+
     static var identifier: String = "CategorySectionViewCell"
 
     weak var delegate: CategorySectionViewCellDelegateProtocol?
-    
+
     private var categoryModel: MMOListByCategoryModel?  {
         didSet {
             collectionView.reloadData()
         }
     }
-    
+
     private lazy var titleLabel: UILabel = UILabel()
-    
+
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 170, height: 170)
@@ -28,18 +28,18 @@ final class CategorySectionViewCell: UICollectionViewCell {
         collection.translatesAutoresizingMaskIntoConstraints = false
         return collection
     }()
-    
-    
+
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         collectionView.delegate = self
         collectionView.dataSource = self
         setupViews()
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) { nil }
-    
+
     // MARK: Setup Methods
     func setupCell(using model: MMOListByCategoryModel) {
         titleLabel.text = model.genre.rawValue
@@ -51,7 +51,7 @@ extension CategorySectionViewCell: UICollectionViewDataSource, UICollectionViewD
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         categoryModel?.listOfMMOs.count ?? 0
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SingleImageViewCell.identifier, for: indexPath) as? SingleImageViewCell else {
             return .init()
@@ -60,31 +60,31 @@ extension CategorySectionViewCell: UICollectionViewDataSource, UICollectionViewD
         return cell
     }
 
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            guard let model = categoryModel?.listOfMMOs[indexPath.row] else { return }
-            let vc = MMOCompleteInformationViewController(model: model)
-            delegate?.presentInformationScreen(viewController: vc)
-//            present(vc, animated: true)
-        }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let model = categoryModel?.listOfMMOs[indexPath.row] else { return }
+        let vc = MMOCompleteInformationViewController(model: model)
+        delegate?.presentInformationScreen(viewController: vc)
+        //            present(vc, animated: true)
+    }
 }
 
 // MARK: ViewConfiguration
 extension CategorySectionViewCell: ViewConfiguration {
-    
+
     func configViews() {
-        
+
     }
-    
+
     func buildViews() {
         contentView.addSubview(titleLabel)
         contentView.addSubview(collectionView)
     }
-    
+
     func setupConstraints() {
         titleLabel.anchor(top: contentView.topAnchor,
                           leading: contentView.leadingAnchor,
                           trailing: contentView.trailingAnchor)
-        
+
         collectionView.anchor(top: titleLabel.bottomAnchor,
                               leading: contentView.leadingAnchor,
                               bottom: contentView.bottomAnchor,
